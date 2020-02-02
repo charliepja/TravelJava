03b5e755d16a1f4994d49ed9975cc169aa633721
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-02 22:27:21 +0000

    adds flight class and passes initial tests

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Flight {
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(Plane plane, String flightNumber, String destination, String departureAirport, String departureTime) {
+        this.passengers = new ArrayList<>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+    public int getTotalPassengers() {
+        return this.passengers.size();
+    }
+
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+    private Flight flight1;
+    private Plane plane1;
+    private Passenger passenger1;
+
+    @Before
+    public void before() {
+        this.passenger1 = new Passenger("Charlie", 1);
+        this.plane1 = new Plane(PlaneType.BOEING747);
+        this.flight1 = new Flight(plane1, "ABC123", "PHI", "GLA", "2130");
+    }
+
+    @Test
+    public void initialPassengerCountIsZero() {
+        assertEquals(0, flight1.getTotalPassengers());
+    }
 }
